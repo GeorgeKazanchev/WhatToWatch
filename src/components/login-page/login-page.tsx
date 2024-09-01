a445096e2219bd2418fffc6e9aaccb22876c2899
George Kazanchev <127313091+GeorgeKazanchev@users.noreply.github.com>
2024-09-01 12:30:06 +0300

    Добавлен компонент страницы авторизации

---
+++ src/components/login-page/login-page.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import * as styles from './login-page.module.scss';
+import { Header } from '../header/header';
+import { Footer } from '../footer/footer';
+
+export const LoginPage: React.FC = () => {
+    return (
+        <div className={styles.page}>
+            <Header
+                isAuthorized={false}
+                isMainPage={false}
+                isUserPage={true}
+            />
+
+            <div className={styles.singIn}>
+                <form action="#" className={styles.form}>
+                    <div className={styles.fields}>
+                        <div className={styles.field}>
+                            <input className={styles.input} type="email" placeholder="Email address" name="user-email" id="user-email" />
+                            <label className={styles.visuallyHidden} htmlFor="user-email">Email address</label>
+                        </div>
+                        <div className={styles.field}>
+                            <input className={styles.input} type="password" placeholder="Password" name="user-password" id="user-password" />
+                            <label className={styles.visuallyHidden} htmlFor="user-password">Password</label>
+                        </div>
+                    </div>
+                    <div>
+                        <button className={styles.signInButton} type="submit">Sign in</button>
+                    </div>
+                </form>
+            </div>
+
+            <Footer
+                isMainPage={false}
+            />
+        </div>
+    );
+};
