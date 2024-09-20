923c72dcec5a3ce69a92e34b3adadd10bcfa6ef3
George Kazanchev <127313091+GeorgeKazanchev@users.noreply.github.com>
2024-09-20 13:38:56 +0300

    Доработана вёрстка страницы с ошибкой "404"

--- src/components/not-found-page/not-found-page.tsx
+++ src/components/not-found-page/not-found-page.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import * as styles from './not-found-page.module.scss';
+import { Link } from 'react-router-dom';
 
 export const NotFoundPage: React.FC = () => {
     return (
-        <section className={styles.container}>
-            <p className={styles.message}>404 Not Found</p>
-        </section>
+        <div className={styles.container}>
+            <section className={styles.content}>
+                <p className={styles.message}>Page not found!</p>
+                <Link to='/' className={styles.link}>Go to the main page</Link>
+            </section>
+        </div>
     );
 };
