7ffd40e85739d2bb7da91addf287220ec5308e3f
George Kazanchev <127313091+GeorgeKazanchev@users.noreply.github.com>
2024-09-22 13:13:55 +0300

    Добавлена страница "My List" со списком фильмов "к просмотру"

--- src/pages/my-list-page/ui/my-list-page.tsx
+++ src/pages/my-list-page/ui/my-list-page.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import * as styles from './my-list-page.module.scss';
+import { useAppSelector } from '../../../shared/hooks/redux-typed-hooks';
+import { selectFavoriteFilms } from '../../../store/reducers/films/films';
+import { Header } from '../../../entities/header';
+import { UserBlock } from '../../../entities/user-block';
+import { Catalog } from '../../../features/catalog';
+import { Footer } from '../../../entities/footer';
 
 export const MyListPage: React.FC = () => {
+    const favoriteFilms = useAppSelector(selectFavoriteFilms);
+
     return (
-        <div>
-            My List Page Template
+        <div className={styles.page}>
+            <Header
+                isUserPage={true}
+                title='My list'
+            >
+                <UserBlock />
+            </Header>
+
+            <Catalog
+                films={favoriteFilms}
+            />
+
+            <Footer
+                isMainPage={false}
+            />
         </div>
     )
 };
--- src/store/reducers/films/films.ts
+++ src/store/reducers/films/films.ts
@@ -26,11 +26,14 @@ export const filmsSlice = createSlice({
         selectAllFilms: (state) => state.films,
         selectFilmById: (state, filmId: number) => {
             return state.films.find((film) => film.id === filmId);
+        },
+        selectFavoriteFilms: (state) => {
+            return state.films.filter((film) => film.isFavorite);
         }
     }
 });
 
 export default filmsSlice.reducer;
-export const { selectAllFilms, selectFilmById } = filmsSlice.selectors;
+export const { selectAllFilms, selectFilmById, selectFavoriteFilms } = filmsSlice.selectors;
 export const { setPromoFilm, loadFilms } = filmsSlice.actions;
 export type { FilmsState };
