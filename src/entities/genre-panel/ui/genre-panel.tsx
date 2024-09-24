ba4dbcefdc16956cb06ace2aef253d1f9e2463f7
George Kazanchev <127313091+GeorgeKazanchev@users.noreply.github.com>
2024-09-24 18:47:51 +0300

    Ограничено количество одновременно отображаемых жанров

--- src/entities/genre-panel/ui/genre-panel.tsx
+++ src/entities/genre-panel/ui/genre-panel.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import * as styles from './genre-panel.module.scss';
+import { MAX_SHOWN_GENRES } from '../../../shared/consts/films';
 
 type Props = {
     genres: string[]
 };
 
 export const GenrePanel: React.FC<Props> = ({ genres }) => {
+    const shownGenres = genres.slice(0, MAX_SHOWN_GENRES);
+
     return (
         <ul className={styles.genres}>
             <li className={`${styles.genre} ${styles.genreActive}`}>
                 <a href="#" className={styles.genreLink}>All genres</a>
             </li>
 
-            {genres.map((genre) =>
+            {shownGenres.map((genre) =>
                 <li key={genre} className={styles.genre}>
                     <a href="#" className={styles.genreLink}>{genre}</a>
                 </li>
--- src/shared/consts/films.ts
+++ src/shared/consts/films.ts
@@ -1,1 +1,2 @@
 export const SIMILAR_FILMS_COUNT = 4;
+export const MAX_SHOWN_GENRES = 9;
